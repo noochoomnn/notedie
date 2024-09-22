44c0fc81aa76e3954ede860671039f02890e5b50
noochoom <nontawat.n167@gmail.com>
2024-09-22 14:56:55 +0700

    refactor & add natural minor scale

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,160 @@
-const NOTES: [&str; 12] = [
-    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
-];
+fn normalize_distance(mut distance: usize) -> usize {
+    if distance > 11 {
+        distance = distance % 12;
+    }
+    distance
+}
 
-#[derive(Clone, Copy)]
-pub enum Interval {
+pub enum Step {
+    None,
     Whole,
     Half,
 }
 
-impl Interval {
-    pub fn unit(self) -> usize {
+impl Step {
+    pub fn usize(&self) -> usize {
         match self {
-            Interval::Half => 1,
-            Interval::Whole => 2,
+            Step::None => 0,
+            Step::Half => 1,
+            Step::Whole => 2,
         }
     }
 }
 
-const MAJOR_FORMULAR: [Interval; 6] = [
-    Interval::Whole,
-    Interval::Whole,
-    Interval::Half,
-    Interval::Whole,
-    Interval::Whole,
-    Interval::Whole,
+#[derive(Clone, Copy)]
+pub enum Accidental {
+    Natural,
+    Flat,
+    Sharp,
+}
+
+const SHARP_NOTES: [&str; 12] = [
+    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
 ];
 
-pub fn get_major_notes(key: usize) {
-    let mut corsor_idx = key;
+const FLAT_NOTES: [&str; 12] = [
+    "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B",
+];
 
-    let mut notes: Vec<String> = Vec::new();
-    notes.push(NOTES[corsor_idx].into());
+struct Note {
+    str: String,
+    index: usize,
+}
 
-    for i in 0..6 {
-        let interval = MAJOR_FORMULAR[i].clone().unit();
-        corsor_idx += interval;
-
-        if corsor_idx > 11 {
-            corsor_idx -= 12;
+impl Note {
+    pub fn from_str(str: &str) -> Note {
+        Note {
+            str: String::from(str),
+            index: Note::index_from_str(str),
         }
-
-        notes.push(String::from(NOTES[corsor_idx as usize]));
     }
 
-    println!("{}", String::from(NOTES[key]));
-    println!("{:?}", notes);
+    pub fn from_distance(distance: usize, accidental: Accidental) -> Note {
+        let index = normalize_distance(distance);
+        let str = match accidental {
+            Accidental::Natural | Accidental::Sharp => String::from(SHARP_NOTES[index]),
+            Accidental::Flat => String::from(FLAT_NOTES[index]),
+        };
+
+        Note { str, index }
+    }
+
+    pub fn to_string(&self) -> String {
+        self.str.clone()
+    }
+
+    fn index_from_str(str: &str) -> usize {
+        match str {
+            "C" => 0,
+            "C#" => 1,
+            "Db" => 1,
+            "D" => 2,
+            "D#" => 3,
+            "Eb" => 3,
+            "E" => 4,
+            "F" => 5,
+            "F#" => 6,
+            "Gb" => 6,
+            "G" => 7,
+            "G#" => 8,
+            "Ab" => 8,
+            "A" => 9,
+            "A#" => 10,
+            "Bb" => 10,
+            "B" => 11,
+            _ => 0,
+        }
+    }
+    fn index(&self) -> usize {
+        self.index.clone()
+    }
+}
+
+const MAJOR_STRUCTURE: [Step; 7] = [
+    Step::None,
+    Step::Whole,
+    Step::Whole,
+    Step::Half,
+    Step::Whole,
+    Step::Whole,
+    Step::Whole,
+];
+
+const NATURAL_MINOR_STRUCTURE: [Step; 7] = [
+    Step::None,
+    Step::Whole,
+    Step::Half,
+    Step::Whole,
+    Step::Whole,
+    Step::Half,
+    Step::Whole,
+];
+
+fn half_steps_from_scale(scale: String) -> Vec<usize> {
+    let mut half_steps: Vec<usize> = Vec::new();
+    let scale_ref = match scale.as_str() {
+        "major" => MAJOR_STRUCTURE,
+        "minor" => NATURAL_MINOR_STRUCTURE,
+        _ => MAJOR_STRUCTURE,
+    };
+
+    let mut current_step: usize = 0;
+    for step in scale_ref.iter() {
+        current_step += step.usize();
+        half_steps.push(current_step.clone());
+    }
+
+    half_steps
+}
+
+pub fn major_scale(root: String, accidental: Accidental) -> Vec<String> {
+    let root_note = Note::from_str(root.as_str());
+
+    let half_steps = half_steps_from_scale(String::from("major"));
+    let mut notes: Vec<String> = Vec::new();
+
+    for step in half_steps.iter() {
+        let distance = root_note.index() + step;
+        let note = Note::from_distance(distance, accidental.clone());
+
+        notes.push(note.to_string());
+    }
+
+    notes
+}
+
+pub fn natural_minor_scale(root: String, accidental: Accidental) -> Vec<String> {
+    let root_note = Note::from_str(root.as_str());
+
+    let half_steps = half_steps_from_scale(String::from("minor"));
+    let mut notes: Vec<String> = Vec::new();
+
+    for step in half_steps.iter() {
+        let distance = root_note.index() + step;
+        let note = Note::from_distance(distance, accidental.clone());
+
+        notes.push(note.to_string());
+    }
+
+    notes
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    notedie::get_major_notes(6);
+    // notedie::get_major_notes(6);
+    let major_notes = notedie::major_scale(String::from("C"), notedie::Accidental::Natural);
+    let natural_minor_notes =
+        notedie::natural_minor_scale(String::from("A"), notedie::Accidental::Natural);
+    println!("Major Scale: {:?}", major_notes);
+    println!("Natural Minor Scale {:?}", natural_minor_notes);
 }
